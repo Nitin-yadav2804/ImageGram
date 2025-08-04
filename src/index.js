85828bfe0eee297b5deb04020af79963b8a29465
Nitin Yadav <nitinyadavsapla1@gmail.com>
2025-08-04 15:58:11 +0530

    Applied rate limit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
 import apiRouter from './routers/apiRouter.js';
+import { rateLimit } from 'express-rate-limit';
 
 const PORT = 3000; // Port number
  
 const app = express(); // Create express app server instance
+
+const limiter = rateLimit({
+    windowMs: 0.5 * 60 * 1000, // 30 seconds
+    max: 5 // limit each IP to 5 requests per windowMs
+});
+
+app.use(limiter); // apply rate limiter to all requests
 
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(express.text()); // Middleware to parse text request bodies
