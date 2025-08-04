8b224426972c41b441688568b81b6e03a0525049
Nitin Yadav <nitinyadavsapla1@gmail.com>
2025-08-04 11:20:49 +0530

    Registration Implemented

--- src/routers/v1/user.js
+++ src/routers/v1/user.js
@@ -1,10 +1,13 @@
 // After /users the remaing part of url is handled here
 
 import express from "express";
-import { getProfile } from "../../controllers/userController.js";
+import { getProfile, signup } from "../../controllers/userController.js";
+import { zodSignupSchema } from "../../validators/zodSignupSchema.js";
+import { validate } from "../../validators/zodValidator.js";
 
 const router = express.Router();
 
 router.get("/profile", getProfile);
+router.post('/signup', validate(zodSignupSchema), signup);
 
 export default router;
---
+++ src/services/userService.js
@@ -0,0 +1,19 @@
+import { createUser } from "../repositories/userRepository.js"
+
+export const signupUserService = async (user) => {
+    try {
+        const newUser = await createUser(user);
+    
+        return newUser;
+
+    } catch (error) {
+        if(error.name === 'MongoServerError' && error.code === 11000) {
+            throw{
+                status: 400,
+                message: " User with same email or username already exists"
+            }
+        }
+        throw error; 
+        
+    }
+}
